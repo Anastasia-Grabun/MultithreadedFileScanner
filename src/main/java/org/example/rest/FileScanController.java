80f92e37269d992130b6d3a64a6810abdee78ca4
Anastasia Grabun <anastasiamoon20@mail.ru>
2025-06-26 07:00:09 +0300

    ref: controller

--- src/main/java/org/example/rest/FileScanController.java
+++ src/main/java/org/example/rest/FileScanController.java
@@ -2,13 +2,12 @@ package org.example.rest;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.example.dto.ScanRequestDTO;
 import org.example.dto.ScanResultDTO;
 import org.example.services.ScannerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @RestController
 @RequestMapping("/scan/api/v1")
@@ -17,11 +16,9 @@ public class FileScanController {
 
     private final ScannerService scannerService;
 
-
-    @GetMapping("/scan")
-    public ScanResultDTO scan(@RequestParam String path, @RequestParam String mask) {
-        List<String> result = scannerService.scan(path, mask);
-        return new ScanResultDTO(result);
+    @GetMapping
+    public ScanResultDTO scan(ScanRequestDTO scanRequestDTO) {
+        return scannerService.scan(scanRequestDTO);
     }
 
 }
